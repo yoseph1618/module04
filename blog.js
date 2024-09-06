870a8f9c359e0d023a64280ce6b22f5db0736764
Luke Skywalker <yoseph16181@gmail.com>
2024-09-05 20:31:20 -0500

    Restart on code.

---
+++ blog.js
@@ -0,0 +1,11 @@
+// TODO: Create a variable that selects the main element, and a variable that selects the back button element
+
+// TODO: Create a function that builds an element and appends it to the DOM
+
+// TODO: Create a function that handles the case where there are no blog posts to display
+
+// TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+
+// TODO: Call the `renderBlogList` function
+
+// TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
---
+++ form.js
@@ -0,0 +1,5 @@
+// TODO: Create a variable that selects the form element
+
+// TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the `redirectPage` function. If the form is submitted with missing data, display an error message to the user.
+
+// TODO: Add an event listener to the form on submit. Call the function to handle the form submission.
